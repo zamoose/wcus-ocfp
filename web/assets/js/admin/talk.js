efd38989d4b780cd490243ef8e085da0c667f99d
Doug Stewart <zamoose@gmail.com>
2016-06-05 23:41:23 -0400

    Initial checkin of Twig templates and web root files.

---
+++ web/assets/js/admin/talk.js
@@ -0,0 +1,93 @@
+function Talk(id, $el) {
+  this.$el = $el;
+  this.id = id;
+  this.baseUrl = '/admin/talks/';
+};
+
+Talk.prototype.favorite = function() {
+  var _this = this;
+  var url = this.baseUrl + this.id + '/favorite';
+  var data = { id: this.id };
+
+  if (this.$el.find('i').hasClass('admin-icon--selected')) {
+    data.delete = true;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    success: function() {
+      _this.$el.find('i').toggleClass('admin-icon--selected');
+    },
+    error: _this.onError
+  });
+};
+
+Talk.prototype.select = function() {
+  var _this = this;
+  var url = this.baseUrl + this.id + '/select';
+  var data = { id: this.id };
+
+  if (this.$el.find('i').hasClass('admin-icon--selected')) {
+    data.delete = true;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    success: function() {
+      _this.$el.find('i').toggleClass('admin-icon--selected');
+    },
+    error: _this.onError
+  });
+};
+
+Talk.prototype.rate = function(rating) {
+  var _this = this;
+  var url = this.baseUrl + this.id + '/rate';
+  var data = { id: this.id, rating: rating };
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    success: function() {
+      if (rating === -1) {
+        $('#talk-downvote-' + _this.id + ' i').addClass('admin-icon--selected');
+        $('#talk-upvote-' + _this.id + ' i').removeClass('admin-icon--selected');
+      }
+
+      if (rating === 1) {
+        $('#talk-upvote-' + _this.id + ' i').addClass('admin-icon--selected');
+        $('#talk-downvote-' + _this.id + ' i').removeClass('admin-icon--selected');
+      }
+    },
+    error: _this.onError
+  });
+};
+
+Talk.prototype.onError = function(xhr, status, errorMessage) {
+  console.log(status + ': ' + errorMessage);
+};
+
+// Add Listeners
+$('.js-talk-rating').on('click', function(e) {
+    var talk = new Talk($(this).data('id'), $(this));
+    var rating = $(this).data('rating');
+    e.preventDefault();
+    talk.rate(rating);
+});
+
+$('.js-talk-favorite').on('click', function(e) {
+    var talk = new Talk($(this).data('id'), $(this));
+    e.preventDefault();
+    talk.favorite();
+});
+
+$('.js-talk-select').on('click', function(e) {
+    var talk = new Talk($(this).data('id'), $(this));
+    e.preventDefault();
+    talk.select();
+});
